e14f6bad7c880dd8e1fbcd175cbd92f18608112c
paul-kibethi <2000kibethipaul@gmail.com>
2024-01-23 14:20:52 +0300

    Passed all test

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+// Multiply
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+console.log(multiply);
+
+
+// Random
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+// mod
+let num3 = 40;
+let num4 = 6;
+
+let mod = num3 % num4;
+console.log(mod);
+
+// max
+let max = Math.max(1, 2, 3, 4, 5, 10, 20);
+console.log(max);
